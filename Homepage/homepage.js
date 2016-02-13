ae9f00de93db3bd0e82c1285a85fbddaf5092665
ChaseHayes <chasehayes22@gmail.com>
2016-02-12 23:56:50 -0500

    finished up the site

--- Homepage/homepage.js
+++ Homepage/homepage.js
@@ -1,2 +1,13 @@
-var now = new Date();
-document.write(now);
+$(document).ready(function(){
+
+	//hide elements so they can fade in
+	$("h1").hide();
+	$("h2").hide();
+	$("#clockdiv").hide();
+
+	//fade in elements here
+	$("h1").fadeIn(1000);
+	$("h2").delay(1000).fadeIn(1000);
+	$("#clockdiv").delay(2000).fadeIn(1000);
+	
+});
